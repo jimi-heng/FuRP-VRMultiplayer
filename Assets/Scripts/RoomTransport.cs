30e948a944203be2762ec1b586764e023d7f9156
吉米亨 <erzhuzi2023@outlook.com>
2025-07-25 01:10:05 +0800

    Update VOLCANO

--- Assets/Scripts/RoomTransport.cs
+++ Assets/Scripts/RoomTransport.cs
@@ -7,10 +7,29 @@ public class RoomTransport : MonoBehaviour
     public Transform room1;
     public Transform room2;
     public Transform room3;
+    public Transform room4;
 
     public void Transform1()
     {
         Vector3 offset = head.position - origin.position;
-        origin.position = room1.position-offset;
+        origin.position = room1.position - offset;
+    }
+
+    public void Transform2()
+    {
+        Vector3 offset = head.position - origin.position;
+        origin.position = room2.position - offset;
+    }
+
+    public void Transform3()
+    {
+        Vector3 offset = head.position - origin.position;
+        origin.position = room3.position - offset;
+    }
+
+    public void Transform4()
+    {
+        Vector3 offset = head.position - origin.position;
+        origin.position = room4.position - offset;
     }
 }
